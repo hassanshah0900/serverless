4ad75cd8ee312799cfc1a4cc6f70e9cf06068396
Syed Muhammad Hassan Shah <hassan@4iisolutions.com>
2026-07-05 18:15:07 +0500

    Wire product CRUD routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { ProductDetail } from './products/product-detail/product-detail';
+import { ProductForm } from './products/product-form/product-form';
+import { ProductList } from './products/product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: 'products',
+    component: ProductList,
+  },
+  {
+    path: 'products/new',
+    component: ProductForm,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetail,
+  },
+  {
+    path: 'products/:id/edit',
+    component: ProductForm,
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+  },
+];
